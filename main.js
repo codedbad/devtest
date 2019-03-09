60bf0e956b9c923ea48309dad2120f1f0d6ea361
Rustams Šteinbergs <47445696+codedbad@users.noreply.github.com>
2019-03-09 21:01:39 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 function createDivs() {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 1; i < 101; i++) {
+        var f = i % 3 == 0, b = i % 5 == 0;
         let div = document.createElement("div");
-        div.setAttribute("id", "my-div-" + (i + 1));
-        let divText = document.createTextNode("Hello World " + (i + 1));
+        div.setAttribute("id", "my-div-" + (i), "class", "bck-" + (f ? b ? "first" : "second" : b ? "third" : i));
+        div.setAttribute("class", "bck-" + (f ? b ? "first" : "second" : b ? "third" : i));
+        let divText = document.createTextNode(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : i);
         div.appendChild(divText);
         let divParent = document.getElementById("container");
         divParent.appendChild(div);
+
     }
 }
-function removeDiv() {
-    var elem = document.getElementById('my-div-1');
-    elem.parentNode.removeChild(elem);
-    return false;
+function removeDivs() {
+    document.getElementById('container').textContent = '';
 }
